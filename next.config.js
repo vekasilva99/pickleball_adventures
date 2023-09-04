363c5b6bb235e56ae6b31d4f3b962076de60f16e
Valeska Silva <vekasilva99@gmail.com>
2023-09-04 11:52:23 +0100

    Prueba2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        DB_URI:"mongodb://localhost:27017/pickelball-adventures",
-        //DB_URI:"mongodb+srv://pickleball-adevntures:TyynIs8itnzqGk0R@pickleballadventures.lkehhod.mongodb.net/?retryWrites=true&w=majority",
-        WEBAPP_URL:"http://localhost:3000/",
-        // WEBAPP_URL:"https://pickleball-adventures.vercel.app/"
+        // DB_URI:"mongodb://localhost:27017/pickelball-adventures",
+        DB_URI:"mongodb+srv://pickleball-adevntures:TyynIs8itnzqGk0R@pickleballadventures.lkehhod.mongodb.net/?retryWrites=true&w=majority",
+        // WEBAPP_URL:"http://localhost:3000/",
+        WEBAPP_URL:"https://pickleball-adventures.vercel.app/"
     }
 }
 
