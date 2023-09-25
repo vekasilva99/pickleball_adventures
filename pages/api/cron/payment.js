1174fbc40bda5987cd09e07769110ec8af617ae4
Valeska Silva <vekasilva99@gmail.com>
2023-09-25 15:19:03 -0400

    j

--- pages/api/cron/payment.js
+++ pages/api/cron/payment.js
@@ -1,10 +1,8 @@
 import { axios } from 'axios'
 
 
-// zod env type checking
-import { env } from 'env'
 
 export async function GET() {
   console.log("cronnnnnn") 
-  console.log(env)
+  console.log(process.env.WEBAPP_UR)
 }
